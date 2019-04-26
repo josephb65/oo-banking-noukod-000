faa9fc71b18caf088158ccfc952aff18ba332d3a
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-26 20:01:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,13 +18,21 @@ class Transfer
   end 
   
   def execute_transaction
-    if @sender.balance > @amount && @status =="pending"
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
-      @receiver.balance -= @amount
-      @status += "complete"
+      @receiver.balance += @amount
+      @status = "complete"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    end 
-  end 
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
